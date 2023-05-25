bea800163a67b97e72d32186831ec24d2edfd489
Aleksandr <sanchasyo@mail.ru>
2023-05-25 10:41:52 +0300

    chore/add global ant design styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
-import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
 
+import { ConfigProvider, Layout } from "antd";
 import "~/styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
@@ -12,7 +13,24 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ConfigProvider
+        theme={{
+          token: {
+            fontFamily: "Golos-Text,sans-serif",
+            colorPrimary: "#008C46",
+          },
+
+        }}
+      >
+        <Layout
+          style={{
+            backgroundColor: "#1E1E1E",
+            minHeight: "100vh",
+          }}
+        >
+          <Component {...pageProps} />
+        </Layout>
+      </ConfigProvider>
     </SessionProvider>
   );
 };
